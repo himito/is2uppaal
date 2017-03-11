0b527cfebdc6cd0dd89223ca244ba79336afcfcc
Jaime Arias <jaime.arias@inria.fr>
2017-03-11 22:50:12 +0100

    cleaning project

--- Compilers/iscore/_build/src/parser.mli
+++
@@ -1,6 +0,0 @@
-(* This exception is raised by the monolithic API functions. *)
-exception Error
-
-(* The monolithic API. *)
-val main: (Lexing.lexbuf -> Lexer.token) -> Lexing.lexbuf -> (Types.program)
-
